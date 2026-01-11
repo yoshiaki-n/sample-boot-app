363186127019939113f7a0d4e99a16c70a30dbdf
yoshiaki-n <7741267+yoshiaki-n@users.noreply.github.com>
2026-01-11 05:57:36 +0000

    style: Apply Spotless formatting

--- src/e2eTest/java/com/example/samplebootapp/e2e/CluecumberReportRunner.java
+++ src/e2eTest/java/com/example/samplebootapp/e2e/CluecumberReportRunner.java
@@ -4,20 +4,20 @@ import com.trivago.cluecumber.core.CluecumberCore;
 import com.trivago.cluecumber.engine.exceptions.CluecumberException;
 
 public class CluecumberReportRunner {
-    public static void main(String[] args) {
-        if (args.length < 2) {
-            System.err.println("Usage: CluecumberReportRunner <jsonDirectory> <reportDirectory>");
-            System.exit(1);
-        }
-        String jsonDirectory = args[0];
-        String reportDirectory = args[1];
-
-        try {
-            new CluecumberCore.Builder().build().generateReports(jsonDirectory, reportDirectory);
-            System.out.println("Cluecumber report generated in " + reportDirectory);
-        } catch (CluecumberException e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
+  public static void main(String[] args) {
+    if (args.length < 2) {
+      System.err.println("Usage: CluecumberReportRunner <jsonDirectory> <reportDirectory>");
+      System.exit(1);
     }
+    String jsonDirectory = args[0];
+    String reportDirectory = args[1];
+
+    try {
+      new CluecumberCore.Builder().build().generateReports(jsonDirectory, reportDirectory);
+      System.out.println("Cluecumber report generated in " + reportDirectory);
+    } catch (CluecumberException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+  }
 }
--- src/e2eTest/java/com/example/samplebootapp/e2e/SetupSteps.java
+++ src/e2eTest/java/com/example/samplebootapp/e2e/SetupSteps.java
@@ -1,11 +1,12 @@
 package com.example.samplebootapp.e2e;
 
-import io.cucumber.java.en.Given;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import io.cucumber.java.en.Given;
+
 public class SetupSteps {
-    @Given("the E2E test environment is configured")
-    public void the_e2e_test_environment_is_configured() {
-        assertTrue(true);
-    }
+  @Given("the E2E test environment is configured")
+  public void the_e2e_test_environment_is_configured() {
+    assertTrue(true);
+  }
 }
